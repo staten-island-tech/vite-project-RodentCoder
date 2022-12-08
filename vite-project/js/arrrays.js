277ad7826fe5bd5e97d2af0c0c401356f37c078f
RodentCoder <ichen080405@gmail.com>
2022-12-08 13:13:17 -0500

    YESSS

---
+++ vite-project/js/arrrays.js
@@ -0,0 +1,3 @@
+const things = [];
+
+export { array };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,4 +2,5 @@ import "../styles/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
+
 AOS.init();
