e21a29ee7c08fbc599d143fd2f0c4867d9ad198c
RodentCoder <ichen080405@gmail.com>
2022-12-13 18:45:32 -0500

    COOL

--- vite-project/js/arrays.js
+++ vite-project/js/arrays.js
@@ -2,21 +2,25 @@ const Monkeys = [
   {
     name: "Dart Monkey",
     class: "Primary",
+    img: "https://static.wikia.nocookie.net/b__/images/b/b2/000-DartMonkey.png/revision/latest?cb=20190522014750&path-prefix=bloons",
     cost: "215",
   },
   {
     name: "Ninja Monkey",
     class: "Magic",
+    img:"https://static.wikia.nocookie.net/b__/images/2/28/000-NinjaMonkey.png/revision/latest?cb=20190522015010&path-prefix=bloons",
     cost: "540",
   },
   {
     name: "Dartling Gunner",
     class: "Military",
+    img:"https://static.wikia.nocookie.net/b__/images/f/f3/000-DartlingGunner.png/revision/latest?cb=20201203034034&path-prefix=bloons",
     cost: "920",
   },
   {
     name: "Banana Farm",
     class: "Support",
+    img:"https://static.wikia.nocookie.net/b__/images/c/cb/000-BananaFarm.png/revision/latest?cb=20190522014608&path-prefix=bloons",
     cost: "1325",
   },
 ];
