4532f8196cb160f4c1d225017b3376a62787fe0e
RodentCoder <ichen080405@gmail.com>
2022-12-14 13:15:11 -0500

    UH OkAAA

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -11,6 +11,15 @@ console.log(Monkeys);
 
 function baseMonkeys() {
   Monkeys.forEach((el) => {
-    Doms.items.insertAdjacentHTML("afterbegin", ``);
+    Doms.items.insertAdjacentHTML(
+      "afterbegin",
+      `
+    <div class="card">
+    <h2 class="item">${el.name}</h2>
+    <img class="monkey" src="${el.img}/>
+    <<h2 class="class">${el.class}</h2>
+    <h3 class="cost">${el.cost}</h3>
+    </div>`
+    );
   });
 }
